3d427073796ee60b8638cbeca9c3696ca9050847
Vic-uchizo <vpradelm@gmail.com>
2026-05-19 16:02:56 -0400

    correction erreur enigma

--- enigma.py
+++ enigma.py
@@ -13,6 +13,6 @@ def enigma_dechiffrer(msg,cles):
     msg_dechiffrer=''
     for i in range(len(msg)):
         index = i % len(cles)
-        msg_dechiffrer += dechiffrer(msg[i], cles[index])
+        msg_dechiffrer -= dechiffrer(msg[i], cles[index])
     return msg_dechiffrer
 
