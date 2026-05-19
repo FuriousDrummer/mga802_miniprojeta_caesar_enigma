d090585604a8ba8f0b50cd06ab4c669a053739a6
kille <killegavre@gmail.com>
2026-05-19 15:06:33 -0400

    fonction chiffrer et dechiffrer v2

--- cesar.py
+++ cesar.py
@@ -1,4 +1,4 @@
-def dechiffrer (message_brut = "veni, vidi, vici!", cle = 42) :
+def chiffrer (message_brut = "veni, vidi, vici!", cle = 42) :
     import string
     alphabet = string.ascii_lowercase
     # 'abcdefghijklmnopqrstuvwxyz'
@@ -14,4 +14,18 @@ def dechiffrer (message_brut = "veni, vidi, vici!", cle = 42) :
 
     return message_traite
 
-print(dechiffrer())
+def dechiffrer (message_brut = "veni, vidi, vici!", cle = 42) :
+    import string
+    alphabet = string.ascii_lowercase
+    # 'abcdefghijklmnopqrstuvwxyz'
+
+    message_traite = ''
+    for letter in message_brut :
+        if letter in alphabet :
+            position_initiale = alphabet.find(letter)
+            position_finale = (position_initiale - cle) % len(alphabet)
+            message_traite += alphabet[position_finale]
+        else :
+            message_traite += letter
+
+    return message_traite
