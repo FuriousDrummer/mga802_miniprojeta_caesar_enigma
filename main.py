55bf59c421616cca5a040a082c37c9ae43489fac
kille <killegavre@gmail.com>
2026-05-26 10:26:45 -0400

    Ajout du traitement séparé de la clé dans la fonction main pour éviter les répétitions Ajout de vérifications sur la présence de la clé lors du chiffrement ou déchiffrement Ajout de commentaires

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+'''
+Ce programme permet de chiffrer, déchiffrer et bruteforce des messages avec le code césar ou un code énigma simplifié.
+Il est possible d'appeler le programme pour avoir une interface ou appeler le fichier avec directement les arguments
+pour une exécution plus rapide.
+'''
+
 from orchestrer import orchestrer, normaliser
 import argparse # pour utiliser argparse
 from cesar import chiffrer, dechiffrer
@@ -7,32 +13,37 @@ from brute_enigma import brute_force_enigma
 import sys #pour comparer l'appel dans la console
 
 def main():
-	# si il n'y a pas d'arguments, le mode interactif est lance
+	"""
+	Fonction principale qui analyse les arguments de la ligne de commande.
+	"""
+
+	# s'il n'y a pas d'arguments (juste le nom du script), le mode interactif est lancé
 	if len(sys.argv) == 1:
 		orchestrer()
 		return
 
-	# sinon configuration de argparse
+	# sinon la configuration de argparse est lancée
 	parser = argparse.ArgumentParser(description="Outil de chiffrement César et Enigma.")
 
-	# arguments principaux
+	# --- arguments principaux obligatoires ---
 	parser.add_argument("mode", choices=["cesar", "enigma"], help="Mode de chiffrement : cesar ou enigma")
 	parser.add_argument("action", choices=["chiffrer", "dechiffrer", "bruteforce"], help="Action a effectuer : chiffrer, dechiffrer, bruteforce")
 	parser.add_argument("message", help="Ecrire le texte a traiter ou le nom du fichier + -f")
 
-	# arguments optionnels
+	# --- arguments optionnels ---
 	parser.add_argument("-c", "--cle", help="La clé (ex: '-c 42' pour cesar, '-c 7-16-9' pour enigma). Requise sauf pour bruteforce.")
 	parser.add_argument("-f", "--fichier", action="store_true", help="Indique que l'argument 'texte' est un chemin de fichier.")
 
 	try:
 		args = parser.parse_args()
 	except SystemExit:
-		# argparse a détecté une erreur et essaie de quitter le script.
+		# interception d'une erreur d'entrée d'arguments
+		# affiche un message explicatif puis quitte le script proprement
 		print("La commande tapée est invalide ou incomplète.")
 		print("Tapez 'python main.py -h' pour afficher le manuel d'aide complet.")
-		return  # On quitte le script proprement
+		return
 
-	# check le fichier
+	# --- traitement de l'entrée (texte ou fichier) ---
 	message = args.message
 	if args.fichier:
 		try:
@@ -43,61 +54,56 @@ def main():
 			sys.exit(1)
 	message = normaliser(message)
 
+	# vérification de l'existence de la clé si l'action n'est pas un bruteforce
+	if args.action in ["chiffrer", "dechiffrer"] and not args.cle:
+		print("Erreur : La clé (-c) est requise pour chiffrer ou déchiffrer.")
+		return
+	elif args.mode == "cesar" and args.action in ["chiffrer", "dechiffrer"]:
+		cle = args.cle
+		try: # conversion de l'entrée STR en entier si possible, sinon retourner un message à l'utilisateur
+			cle = int(cle)
+		except ValueError:
+			print(f"La clé contient une valeur non entière. Réessayez.")
+			return
+	elif args.mode == "enigma" and args.action in ["chiffrer", "dechiffrer"]:
+		# séparation des 3 clés dans un tuple avec vérification de réussite, sinon retourner un message à l'utilisateur
+		cle = args.cle
+		cle = cle.split("-")
+		if len(cle) != 3:
+			print("Il faut exactement 3 clés séparées par '-'. Réessayez.")
+			return
+		try:
+			cle = tuple(int(x) for x in cle)
+		except ValueError:
+			print(f"Les clés contiennent une valeur non entière. Réessayez.")
+			return
 
+
+	# --- execution de l'action dans le mode de chiffrement demandé ---
 	if args.action == "bruteforce":
 		if args.mode == "cesar":
 			cle, resultat, mots_non_identifies = brute_force_cesar(message)
 		else:
-			cle, message, mots_non_identifies = brute_force_enigma(message)
+			cle, resultat, mots_non_identifies = brute_force_enigma(message)
 		print("\n--- Résultat Brute-Force ---")
 		print(f"Clé trouvée   : {cle}")
-		print(f"Déchiffrement : {message}")
+		print(f"Déchiffrement : {resultat}")
 		if mots_non_identifies:
 			print(f"Mots non-identifiés : {mots_non_identifies}")
+
 	elif args.action == "chiffrer":
 		if args.mode == "cesar":
-			cle = args.cle
-			try:
-				cle = int(cle)
-			except ValueError:
-				print(f"La clé contient une valeur non entière. Réessayez.")
-				return
-			message = chiffrer(message, cle)
+			resultat = chiffrer(message, cle)
 		else:
-			cle = args.cle
-			cle = cle.split("-")
-			if len(cle) != 3:
-				print("Il faut exactement 3 clés séparées par '-'. Réessayez.")
-				return
-			try:
-				cle = tuple(int(x) for x in cle)
-			except ValueError:
-				print(f"Les clés contiennent une valeur non entière. Réessayez.")
-				return
-			message = enigma_chiffrer(message, cle)
-		print(f"Le message chiffré est : {message}")
+			resultat = enigma_chiffrer(message, cle)
+		print(f"Le message chiffré est : {resultat}")
+
 	elif args.action == "dechiffrer":
 		if args.mode == "cesar":
-			cle = args.cle
-			try:
-				cle = int(cle)
-			except ValueError:
-				print(f"La clé contient une valeur non entière. Réessayez.")
-				return
-			message = dechiffrer(message, cle)
+			resultat = dechiffrer(message, cle)
 		else:
-			cle = args.cle
-			cle = cle.split("-")
-			if len(cle) != 3:
-				print("Il faut exactement 3 clés séparées par '-'. Réessayez.")
-				return
-			try:
-				cle = tuple(int(x) for x in cle)
-			except ValueError:
-				print(f"Les clés contiennent une valeur non entière. Réessayez.")
-				return
-			message = enigma_dechiffrer(message, cle)
-		print(f"Le message déchiffré est : {message}")
+			resultat = enigma_dechiffrer(message, cle)
+		print(f"Le message déchiffré est : {resultat}")
 
 
 
